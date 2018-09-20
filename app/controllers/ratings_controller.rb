5c9d8ac204ce7f3b11d8fba021f1fdec29be14c6
hvdung14@gmail.com <hvdung14@gmail.com>
2018-09-20 10:55:59 +0700

    Fix issues and improve rate film

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,5 +1,6 @@
 class RatingsController < ApplicationController
   before_action :logged_in_user, only: :create
+  before_action :find_rate, only: [:edit, :update]
 
   def create
     @rate = Rating.new rate_params
@@ -12,9 +13,26 @@ class RatingsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @rate.update rate_params
+      flash[:alert] = t ".rating_updated"
+      redirect_to film_path @rate.film_id
+    else
+      flash[:alert] = t ".rating_updated_error"
+      render "films/show"
+    end
+  end
+
   private
 
   def rate_params
     params.require(:rating).permit(:rate, :film_id).merge user_id: current_user.id
+  end
+
+  def find_rate
+    @rate = current_user.ratings.find_by film_id: params[:id]
   end
 end
